7f0d1938f541478f937252d4e18c0ee9460903fe
sxl <1693847306@qq.com>
2025-03-11 21:53:38 +0800

    feat(kill_service.sh): 批量杀死多个服务器上的服务
    - 新增服务器IP数组和服名数组
    - 使用循环遍历每个服务器和每个服务
    - 优化脚本以支持批量操作
    - 添加日志输出以提高可读性

--- shell/kill_service.sh
+++ shell/kill_service.sh
@@ -2,14 +2,20 @@
 
 #source "$(dirname "$0")/kill.properties"
 REMOTE_USER="root"
-REMOTE_IP="192.168.42.106"
 REMOTE_PASSWORD="root"
-SERVICE_NAME="line-gateway"
 REMOTE_KILL_SCRIPT="/usr/local/jar/kill_process.sh"
 
-# 连接到远程服务器，先执行kill_process.sh，然后启动新的JAR文件
-echo "正在连接到远程服务器..."
-/usr/bin/expect <<EOF
+# 定义服务器IP数组
+REMOTE_IPS=("192.168.42.106" "192.168.42.234")
+# 定义服务名数组
+SERVICE_NAMES=("line-gateway" "search-line-file-service" "search-line-es-core-service")
+
+# 遍历服务器IP数组
+for REMOTE_IP in "${REMOTE_IPS[@]}"; do
+    # 遍历服务名数组
+    for SERVICE_NAME in "${SERVICE_NAMES[@]}"; do
+        echo "正在连接到远程服务器 $REMOTE_IP 杀死服务 $SERVICE_NAME..."
+        /usr/bin/expect <<EOF
 set timeout 20
 spawn ssh $REMOTE_USER@$REMOTE_IP
 expect {
@@ -23,3 +29,6 @@ expect "root@"
 send "exit\r"
 expect eof
 EOF
+        echo "服务 $SERVICE_NAME 在服务器 $REMOTE_IP 上已停止"
+    done
+done
