6e77a093e34855199e4362ebaebbca7ac1398f39
sxl <1693847306@qq.com>
2025-03-18 23:12:15 +0800

    feat(service): 添加 Dubbo 支持并更新相关配置
    - 在 ESServiceApplication 和 FileServiceApplication 中添加 Dubbo 支持- 更新 application.yml 文件，引入 dubbo.yml 配置
    - 在 search-line-service 的 pom.xml 中添加 search-line-dubbo 依赖
    - 新增 dubbo-provider.xml 文件，配置 Dubbo 服务

--- search-line-service/search-line-file-service/src/main/java/top/boking/file/FileServiceApplication.java
+++ search-line-service/search-line-file-service/src/main/java/top/boking/file/FileServiceApplication.java
@@ -1,13 +1,17 @@
 package top.boking.file;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("top.boking.file.mapper")
 @EnableScheduling
+@EnableDubbo
+@ImportResource(locations = {"classpath:dubbo-provider.xml"})
 public class FileServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FileServiceApplication.class, args);
